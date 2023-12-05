55816ae6827344640e8f5615444f617cfa953a42
Snegan29 <pssj2914@gmail.com>
2023-12-05 09:19:33 +0530

    Made Changes

---
+++ kalvium-pronote/src/Component/Input.jsx
@@ -0,0 +1,16 @@
+import  { Component } from 'react'
+
+export class Input extends Component {
+  render() {
+    return (
+        <div id='input'>
+
+        <input type = 'text' placeholder='Enter Text' value={this.props.inputValue} onChange={ (e)=> this.props.handleChange(e.target.value) } />
+        <button onClick={this.props.handleSubmit} >Create</button>
+    
+        </div>
+    )
+  }
+}
+
+export default Input
